4a8cdd5f4864aedc4a0a7d447b329ad881b52662
İhlas Mert <ihlas1mert@gmail.com>
2023-04-19 09:39:06 +0300

    added header section-completed

--- src/components/Header/hero-section/HeroSection.jsx
+++ src/components/Header/hero-section/HeroSection.jsx
@@ -5,18 +5,18 @@ import heroImg from "../../../assets/image.png"
 
 const HeroSection = () => {
   return (
-    <section>
+    <section className='hero__section' id='home'>
         <Container>
             <Row>
                 <Col lg="6" md="6">
                     <div className='hero__content'>
                         <p>Welcome to my world!</p>
                         <h5>Hi</h5>
-                        <h5>I am <span>İhlas Mert</span></h5>
+                        <h2 className='hero-title'>I am <span>İhlas Mert</span></h2>
                         <p>Lorem23232</p>
                         <div className='hero__btns d-flex align-items-center gap-4'>
                             <button className='btn'><a href='#'>Hire Me</a></button>
-                            <button className='btn-2'>Contact</button>
+                            <button className='btn-hire'>Contact</button>
                         </div>
                     </div>
                 </Col>
